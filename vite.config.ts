798848214ef7b071031c500fcfbfabd4910543b8
mis-sales-glitch <mis-sales@agoraccs.com>
2025-11-10 12:59:06 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -21,3 +21,14 @@ export default defineConfig(({ mode }) => {
       }
     };
 });
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'docs'
+  }
+})
